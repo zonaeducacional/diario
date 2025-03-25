a3326aa0e1dd88b153ff000ebaab2f6e3dff6614
Rep <zonaeducacional@gmail.com>
2025-03-25 15:44:37 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Configuração do Firebase (versão compatível)
+const firebaseConfig = {
+  apiKey: "AIzaSyDCrKS17Fle4ycDbvD8lIFjndq1XVeDjEk",
+  authDomain: "diario-243fa.firebaseapp.com",
+  projectId: "diario-243fa",
+  storageBucket: "diario-243fa.appspot.com",
+  messagingSenderId: "836464209379",
+  appId: "1:836464209379:web:c3e0484ad00636c18184d6"
+};
+
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
